7747fd21a376b2c471597bf2cca18e1b9ba19491
Michael Hegner <hegner123@gmail.com>
2019-08-21 12:21:19 -0400

    started psuedo-coding functions and game states. selected characters.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,66 @@
+$( document ).ready(function() {
+  
+  var game = {
+    state:pauseState()
+
+  }
+
+
+function pauseState(){
+  ;
+}
+
+$("#admiral-akbar").click(function(){
+  $("#admiral-akbar").css("border-color","red");
+});
+
+  var admiralAkbar = {
+    health: 100,
+    attack: 25,
+    
+  
+
+  }
+   
+  var boobaFett = {
+    health: 100,
+    attack: 25,
+    
+  }
+
+  var chewbaca = {
+    health: 100,
+    attack: 25,
+    
+  }
+
+  var darthVader = {
+    health: 100,
+    attack: 25,
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
+
+
