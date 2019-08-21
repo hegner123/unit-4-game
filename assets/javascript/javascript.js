450f1c6916b687c662b7d048f50370163b670757
Michael Hegner <hegner123@gmail.com>
2019-08-21 19:11:23 -0400

    edited select function

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,18 +10,34 @@ function pauseState(){
   ;
 }
 
-$("#admiral-akbar").click(function(){
-  $("#admiral-akbar").css("border-color","red");
-});
 
-  var admiralAkbar = {
+var character1 = document.getElementById("admiral-akbar");
+
+
+
+var admiralAkbar = {
     health: 100,
     attack: 25,
     
-  
-
   }
+
+  function select(character) {
+    $(character).click(function() {
+      $(character).css("border-color","red");
+    });
+    $("#darth-vader, #chewbaca, #bobba-fett").click(function () {
+      $(character).css("border-color","black")
+    })
+  }
+
+  
+  
+select(character1);
+
+
    
+
+
   var boobaFett = {
     health: 100,
     attack: 25,
