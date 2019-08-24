976fa567df7e59f223bf1e4920fc82b8640f16e5
Michael Hegner <hegner123@gmail.com>
2019-08-23 22:56:18 -0400

    completed turn based combat function for a single cycle.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,77 +1,75 @@
 $( document ).ready(function() {
+  // main character variables
+  
+var userCharacter = "";
 
-// create div's with images with for loop
+var computerCharacter = "";
 
+// character objects
+  var admiralAkbar = {
+    data:"admiralAkbar",
+    hp: 100,
+    attack: 20,
+    counterAttack: 25,
+    modifier: "",
+    display: "Admiral Akbar",
+  }
 
+  var bobbaFett = {
+    data:"bobbaFett",
+    hp: 100,
+    attack: 20,
+    counterAttack: 25,
+    modifier: "",
+    display: "Bobba Fett",
+  }
+
+  var chewbaca = {
+    data:"chewbaca",
+    hp: 100,
+    attack: 20,
+    counterAttack: 25,
+    modifier: "",
+    display: "Chewbaca"
+  }
+
+  var darthVader = {
+    data:"darthVader",
+    hp: 100,
+    attack: 20,
+    counterAttack: 25,
+    modifier: "",
+    display: "Darth Vader",
+  }
 
 var characters = ["admiralAkbar", "bobbaFett", "chewbaca", "darthVader"];
+var characterObject = [admiralAkbar, bobbaFett, chewbaca, darthVader];
 
+// create div's with images with for loop
 for (i=0;i<characters.length;i++) {
   var characterbtn = $("<div>");
     characterbtn.addClass("chara-button character not-selected " + characters[i]);
-    // characterbtn.attr("character-name", characters[i]);
+    characterbtn.data("data", characters[i]);
     characterbtn.appendTo("#char-sel");
-    console.log(characterbtn);
+    console.log(characterbtn.data());
     characterImage();
     function characterImage() {
       var charImg = $("<img>");
-      charImg.addClass("imgClear");
+      charImg.addClass("imgClear character-image");
       charImg.appendTo(characterbtn);
       charImg.attr("src", "assets/images/" + characters[i] + ".jpg");
     }
 }
 
-var admiralAkbar = document.getElementsByClassName("admiralAkbar");
-var bobbaFett = document.getElementsByClassName("bobbaFett");
-var chewbaca = document.getElementsByClassName("chewbaca");
-var darthVader = document.getElementsByClassName("darthVader");
-
-
-
-  var admiralAkbar = {
-    
-    health: 100,
-    attack: 20,
-    counterAttack:25,
-    modifier: "",
-  }
-
-  var bobbaFett = {
-    
-    health: 100,
-    attack: 20,
-    counterAttack:25,
-    modifier: "",
-  }
-
-  var chewbaca = {
-    
-    health: 100,
-    attack: 20,
-    counterAttack:25,
-    modifier: "",
-  }
-
-  var darthVader = {
-    
-    health: 100,
-    attack: 20,
-    counterAttack:25,
-    modifier: "",
-  }
-
- 
-
-
   function charaSel (){
     $(".character").on("click", function(){
       if (game.step < 1){
-        game.userCharacter = this;
+        userCharacter = $(this).data();
       $(this).addClass("user-chara-sel");
       $(this).removeClass("not-selected");
       $(".not-selected").appendTo("#enemy-select");
       game.state = compCharacter();
-      console.log(game);
+      console.log(userCharacter);
       game.step += 1;
       } else {
         ;
@@ -83,33 +81,43 @@ var darthVader = document.getElementsByClassName("darthVader");
   function compCharacter(){
     $(".character").on("click", function(){
       if (game.step < 2 && ($(this).hasClass("not-selected"))) {
-        game.compCharacter = this;
+        computerCharacter = $(this).data();
       $(this).addClass("comp-chara-sel");
       $(".comp-chara-sel").appendTo("#attack-target");
       game.state = attackState();
-      console.log(game);
+      console.log(computerCharacter);
       game.step += 1;
       } else {
         ;
       }
-      
     })
   }
 
   function attackState(){
     $("#attack-button").on("click", function (){
-      if (game.step < 3) {
-       ;
-        game.step += 1;
-      } else {
-      console.log(game.userCharacter);
-      console.log(game.compCharacter);
+      for (j=0;j<characterObject.length;j++) {
+        console.log(characterObject[j].data);
+        if (userCharacter.data == characterObject[j].data ) {
+          userCharacter = characterObject[j];
+        } else if ( computerCharacter.data == characterObject[j].data) {
+          computerCharacter = characterObject[j];
+        } else {
+          
+          console.log(userCharacter);
+          console.log(computerCharacter);
+        }
       }
-      
-    })
-    ;
-  }
- 
+
+      fightAction();
+      $(".after-action").text(userCharacter.display + " :" + userCharacter.hp + "     " + computerCharacter.display + " :" + computerCharacter.hp);
+
+    });}
+    
+  function fightAction () {
+  userCharacter.hp = userCharacter.hp - computerCharacter.attack;
+  computerCharacter.hp = computerCharacter.hp - userCharacter.attack;
+}
+     
   var game = {
     state: charaSel(),
     userCharacter: "",
